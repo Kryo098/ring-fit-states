fc3ab37c1121cee115786500c88618fe7d1f7b01
Kryo098 <ryo0812eng@gmail.com>
2021-10-19 13:18:29 +0900

    master

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDLcIfRtfktJJ8qPQxNFVSd7kf9X7Nxs6A",
-  authDomain: "ringfit-states.firebaseapp.com",
-  projectId: "ringfit-states",
-  storageBucket: "ringfit-states.appspot.com",
-  messagingSenderId: "521721641426",
-  appId: "1:521721641426:web:3118fc36c8d2aa8878300b",
-  measurementId: "G-TBYM58QQKC"
-};
